918574e8dc41bf1ac7266ca21c4339e91a6ceef5
lingxin.cao <lingxin.cao@uaes.com>
2018-04-08 11:45:53 +0800

    based on the BDP-143 updated on 电池包状态及电量不足提醒 CAN Message 定义

--- src/main/java/com/uaes/esw/gwmc30demo/domain/model/entity/can/B2CanMessage.java
+++ src/main/java/com/uaes/esw/gwmc30demo/domain/model/entity/can/B2CanMessage.java
@@ -3,6 +3,9 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data @Builder
-
 public class B2CanMessage {
+    long unixtimestamp;
+    double Pack_CellSocMax_BMS; //电池系统最高单体SOC
+    double Pack_CellSocMin_BMS; //电池系统最低单体SOC
+    double Pack_ChrgReTime_BMS; //电池系统充电剩余时间
 }
--- src/main/java/com/uaes/esw/gwmc30demo/domain/model/entity/can/B3CanMessage.java
+++ src/main/java/com/uaes/esw/gwmc30demo/domain/model/entity/can/B3CanMessage.java
@@ -3,6 +3,7 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data @Builder
-
 public class B3CanMessage {
+    long unixtimestamp;
+    int Cus_BalcReq; //用户均衡请求指令
 }
