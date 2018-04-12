7f2879ef68c754aaad953f82f1e14797486b9803
lingxin.cao <lingxin.cao@uaes.com>
2018-04-12 18:03:52 +0800

    based on the BDP-143 updated on 更新drivingModel中属性的缺省定义

--- src/main/java/com/uaes/esw/gwmc30demo/constant/DrivingModeConstants.java
+++ src/main/java/com/uaes/esw/gwmc30demo/constant/DrivingModeConstants.java
@@ -15,39 +15,39 @@ public class DrivingModeConstants {
     public final static String DRIVING_MODE_ABB_POW = "POW"; //POWER_MODE
     public final static String DRIVING_MODE_ABB_CST = "CST"; //CUSTOMER_MODE
 
-    public final static int EEM_MAX_SPEED = 0;
-    public final static int EEM_ENERGY_RECOVERY = 0;
-    public final static int EEM_POWER_CORRESPONDING = 0;
-    public final static int EEM_SMOOTHNESS = 0;
-    public final static int EEM_ACCESSORY_PERFORMANCE = 0;
+    public final static int EEM_MAX_SPEED = 1;
+    public final static int EEM_ENERGY_RECOVERY = 1;
+    public final static int EEM_POWER_CORRESPONDING = 1;
+    public final static int EEM_SMOOTHNESS = 1;
+    public final static int EEM_ACCESSORY_PERFORMANCE = 1;
 
-    public final static int ECO_MAX_SPEED = 1;
-    public final static int ECO_ENERGY_RECOVERY = 1;
-    public final static int ECO_POWER_CORRESPONDING = 1;
-    public final static int ECO_SMOOTHNESS = 1;
-    public final static int ECO_ACCESSORY_PERFORMANCE = 1;
+    public final static int ECO_MAX_SPEED = 2;
+    public final static int ECO_ENERGY_RECOVERY = 2;
+    public final static int ECO_POWER_CORRESPONDING = 2;
+    public final static int ECO_SMOOTHNESS = 2;
+    public final static int ECO_ACCESSORY_PERFORMANCE = 2;
 
-    public final static int NOR_MAX_SPEED = 2;
-    public final static int NOR_ENERGY_RECOVERY = 2;
-    public final static int NOR_POWER_CORRESPONDING = 2;
-    public final static int NOR_SMOOTHNESS = 2;
-    public final static int NOR_ACCESSORY_PERFORMANCE = 2;
+    public final static int NOR_MAX_SPEED = 3;
+    public final static int NOR_ENERGY_RECOVERY = 3;
+    public final static int NOR_POWER_CORRESPONDING = 3;
+    public final static int NOR_SMOOTHNESS = 3;
+    public final static int NOR_ACCESSORY_PERFORMANCE = 3;
 
-    public final static int EPM_MAX_SPEED = 3;
-    public final static int EPM_ENERGY_RECOVERY = 3;
-    public final static int EPM_POWER_CORRESPONDING = 3;
-    public final static int EPM_SMOOTHNESS = 3;
-    public final static int EPM_ACCESSORY_PERFORMANCE = 3;
+    public final static int EPM_MAX_SPEED = 4;
+    public final static int EPM_ENERGY_RECOVERY = 4;
+    public final static int EPM_POWER_CORRESPONDING = 4;
+    public final static int EPM_SMOOTHNESS = 4;
+    public final static int EPM_ACCESSORY_PERFORMANCE = 4;
 
-    public final static int POW_MAX_SPEED = 4;
-    public final static int POW_ENERGY_RECOVERY = 4;
-    public final static int POW_POWER_CORRESPONDING = 4;
-    public final static int POW_SMOOTHNESS = 4;
-    public final static int POW_ACCESSORY_PERFORMANCE = 4;
+    public final static int POW_MAX_SPEED = 5;
+    public final static int POW_ENERGY_RECOVERY = 5;
+    public final static int POW_POWER_CORRESPONDING = 5;
+    public final static int POW_SMOOTHNESS = 5;
+    public final static int POW_ACCESSORY_PERFORMANCE = 5;
 
-    public final static int CST_MAX_SPEED = 5;
-    public final static int CST_ENERGY_RECOVERY = 5;
-    public final static int CST_POWER_CORRESPONDING = 5;
-    public final static int CST_SMOOTHNESS = 5;
-    public final static int CST_ACCESSORY_PERFORMANCE = 5;
+    public final static int CST_MAX_SPEED = 6;
+    public final static int CST_ENERGY_RECOVERY = 6;
+    public final static int CST_POWER_CORRESPONDING = 6;
+    public final static int CST_SMOOTHNESS = 6;
+    public final static int CST_ACCESSORY_PERFORMANCE = 6;
 }
