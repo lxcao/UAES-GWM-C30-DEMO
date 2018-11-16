bd9481428987dde68171eac12cdc0f3f76c94874
lingxin.cao <lingxin.cao@uaes.com>
2018-11-16 16:35:07 +0800

    based on the BDP-143 updated on 完成对驾驶循环中故障等级的实例映射

--- src/main/java/com/uaes/esw/gwmc30demo/domain/service/BlackBoxDomainService.java
+++ src/main/java/com/uaes/esw/gwmc30demo/domain/service/BlackBoxDomainService.java
@@ -25,10 +25,14 @@ import com.uaes.esw.gwmc30demo.domain.model.scenario.blackBox.QueryDCBDRes;
 import com.uaes.esw.gwmc30demo.domain.repository.can.ICanRepository;
 import com.uaes.esw.gwmc30demo.domain.repository.vehicle.IVehicleRepository;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.uaes.esw.gwmc30demo.constant.CommonConstants.*;
+import static java.util.stream.Collectors.groupingBy;
 
 public class BlackBoxDomainService {
     static public QueryDCBDRes queryDCBDDomainService(QueryDCBDReq queryDCBDReq){
@@ -49,7 +53,7 @@ public class BlackBoxDomainService {
                 getDrivingCycleByPeriod(queryDateStart, queryDateEnd);
         //获取每一个驾驶循环的故障统计
         drivingCycleSet.forEach(dc -> {
-            dc.setFltGrdSum(calFaultGradeSummaryByDrivingCycle(dc.getPowerOnTimestamp(),
+            dc.setFltGrdSum(calSumFaultGradeSummaryByDrivingCycle(dc.getPowerOnTimestamp(),
                     dc.getPowerOffTimestamp()));
         });
         return drivingCycleSet;
@@ -64,7 +68,155 @@ public class BlackBoxDomainService {
         });
     }
 
-    static FltGrdSum calFaultGradeSummaryByDrivingCycle(long dcStartTime, long dcEndTime){
+    //是否有故障发生
+    static boolean isFaultHappened(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        if(vcu79CanMessagesSet.parallelStream()
+                .filter(v -> v.getAccident_Flg() == 1).findAny().isPresent())
+            return true;
+        return false;
+    }
+
+    //加速踏板故障发生
+    static boolean isAccPdlFltHappened(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        if(vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getAcc_Pdl_Flt_Grd() != 0).findAny().isPresent())
+            return true;
+        return false;
+    }
+    //加速踏板故障发生的等级
+    static Stream<Integer> getAccPdlFltGrd(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getAcc_Pdl_Flt_Grd() != 0)
+                .mapToInt(v -> v.getAcc_Pdl_Flt_Grd()).distinct().boxed();
+    }
+    //加速踏板故障发生的等级及其实例映射
+    static public Map<Integer, List<VCU79CanMessage>> getAccPdlFltGrdAndList(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.stream()
+                .filter(s -> s.getAcc_Pdl_Flt_Grd() != 0)
+                .collect(groupingBy(VCU79CanMessage::getAcc_Pdl_Flt_Grd, Collectors.toList()));
+    }
+
+    //制动踏板故障发生
+    static boolean isBrkPdlFltHappened(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        if(vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getBrk_Pdl_Flt_Grd() != 0).findAny().isPresent())
+            return true;
+        return false;
+    }
+    //制动踏板故障发生的等级
+    static Stream<Integer> getBrkPdlFltGrd(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getBrk_Pdl_Flt_Grd() != 0)
+                .mapToInt(v -> v.getBrk_Pdl_Flt_Grd()).distinct().boxed();
+    }
+    //制动踏板故障发生的等级及其实例映射
+    static Map<Integer, List<VCU79CanMessage>> getBrkPdlFltGrdAndList(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.stream()
+                .filter(s -> s.getBrk_Pdl_Flt_Grd() != 0)
+                .collect(groupingBy(VCU79CanMessage::getBrk_Pdl_Flt_Grd, Collectors.toList()));
+    }
+
+    //档位故障发生
+    static boolean isGearFltHappened(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        if(vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getGear_Flt_Grd() != 0).findAny().isPresent())
+            return true;
+        return false;
+    }
+    //档位故障发生的等级
+    static Stream<Integer> getGearPdlFltGrd(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getGear_Flt_Grd() != 0)
+                .mapToInt(v -> v.getGear_Flt_Grd()).distinct().boxed();
+    }
+    //档位故障发生的等级及其实例映射
+    static Map<Integer, List<VCU79CanMessage>> getGearFltGrdAndList(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.stream()
+                .filter(s -> s.getGear_Flt_Grd() != 0)
+                .collect(groupingBy(VCU79CanMessage::getGear_Flt_Grd, Collectors.toList()));
+    }
+
+    //车速故障发生
+    static boolean isSpdFltHappened(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        if(vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getSpd_Flt_Grd() != 0).findAny().isPresent())
+            return true;
+        return false;
+    }
+    //车速故障发生的等级
+    static Stream<Integer> getSpdFltGrd(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getSpd_Flt_Grd() != 0)
+                .mapToInt(v -> v.getSpd_Flt_Grd()).distinct().boxed();
+    }
+    //车速故障发生的等级及其实例映射
+    static Map<Integer, List<VCU79CanMessage>> getSpdFltGrdAndList(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.stream()
+                .filter(s -> s.getSpd_Flt_Grd() != 0)
+                .collect(groupingBy(VCU79CanMessage::getSpd_Flt_Grd, Collectors.toList()));
+    }
+
+    //驱动电机故障发生
+    static boolean isDrvMtrFltHappened(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        if(vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getDrv_Mtr_Flt_Grd() != 0).findAny().isPresent())
+            return true;
+        return false;
+    }
+    //驱动电机故障发生的等级
+    static Stream<Integer> getDrvMtrFltGrd(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getDrv_Mtr_Flt_Grd() != 0)
+                .mapToInt(v -> v.getDrv_Mtr_Flt_Grd()).distinct().boxed();
+    }
+    //驱动电机故障发生的等级及其实例映射
+    static Map<Integer, List<VCU79CanMessage>> getDrvMtrFltGrdAndList(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.stream()
+                .filter(s -> s.getDrv_Mtr_Flt_Grd() != 0)
+                .collect(groupingBy(VCU79CanMessage::getDrv_Mtr_Flt_Grd, Collectors.toList()));
+    }
+
+    //DCDC故障发生
+    static boolean isDCDCFltHappened(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        if(vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getDCDC_Flt_Grd() != 0).findAny().isPresent())
+            return true;
+        return false;
+    }
+    //DCDC故障发生的等级
+    static Stream<Integer> getDCDCFltGrd(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getDCDC_Flt_Grd() != 0)
+                .mapToInt(v -> v.getDCDC_Flt_Grd()).distinct().boxed();
+    }
+    //DCDC故障发生的等级及其实例映射
+    static Map<Integer, List<VCU79CanMessage>> getDCDCFltGrdAndList(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.stream()
+                .filter(s -> s.getDCDC_Flt_Grd() != 0)
+                .collect(groupingBy(VCU79CanMessage::getDCDC_Flt_Grd, Collectors.toList()));
+    }
+
+    //电池故障发生
+    static boolean isBtyFltHappened(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        if(vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getBty_Flt_Grd() != 0).findAny().isPresent())
+            return true;
+        return false;
+    }
+    //电池故障发生的等级
+    static Stream<Integer> getBtyFltGrd(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.parallelStream()
+                .filter(s -> s.getBty_Flt_Grd() != 0)
+                .mapToInt(v -> v.getBty_Flt_Grd()).distinct().boxed();
+    }
+    //电池故障发生的等级及其实例映射
+    static Map<Integer, List<VCU79CanMessage>> getBtyFltGrdAndList(Set<VCU79CanMessage> vcu79CanMessagesSet){
+        return vcu79CanMessagesSet.stream()
+                .filter(s -> s.getBty_Flt_Grd() != 0)
+                .collect(groupingBy(VCU79CanMessage::getBty_Flt_Grd, Collectors.toList()));
+    }
+
+    static FltGrdSum calSumFaultGradeSummaryByDrivingCycle(long dcStartTime, long dcEndTime){
         Set<VCU79CanMessage> vcu79CanMessagesSet = ICanRepository.
                 getVCU79MessageFromRedisByPeriod(dcStartTime, dcEndTime);
         // 初始化
@@ -73,51 +225,29 @@ public class BlackBoxDomainService {
         Stream<Integer> accPdlFltGrdArray = null, brkPdlFltGrdArray = null, gearFltGrdArray = null, spdFltGrdArray = null;
         Stream<Integer> drvMtrFltGrdArray = null, dCDCFltGrdArray = null, btyFltGrdArray = null;
         //判断是否有故障
-        if(vcu79CanMessagesSet.parallelStream()
-                .filter(v -> v.getAccident_Flg() == 1).findAny().isPresent()){
+        if(isFaultHappened(vcu79CanMessagesSet)){
             fltGrdSum.setHasFlt(true);
             //加速踏板故障等级
-            if(vcu79CanMessagesSet.parallelStream()
-                    .filter(s -> s.getAcc_Pdl_Flt_Grd() != 0).findAny().isPresent())
-                accPdlFltGrdArray = vcu79CanMessagesSet.parallelStream()
-                        .filter(s -> s.getAcc_Pdl_Flt_Grd() != 0)
-                        .mapToInt(v -> v.getAcc_Pdl_Flt_Grd()).distinct().boxed();
+            if(isAccPdlFltHappened(vcu79CanMessagesSet))
+                accPdlFltGrdArray = getAccPdlFltGrd(vcu79CanMessagesSet);
             //制动踏板故障等级
-            if(vcu79CanMessagesSet.parallelStream()
-                    .filter(s -> s.getBrk_Pdl_Flt_Grd() != 0).findAny().isPresent())
-                brkPdlFltGrdArray = vcu79CanMessagesSet.parallelStream()
-                        .filter(s -> s.getBrk_Pdl_Flt_Grd() != 0)
-                        .mapToInt(v -> v.getBrk_Pdl_Flt_Grd()).distinct().boxed();
+            if(isBrkPdlFltHappened(vcu79CanMessagesSet))
+                brkPdlFltGrdArray = getBrkPdlFltGrd(vcu79CanMessagesSet);
             //档位故障等级
-            if(vcu79CanMessagesSet.parallelStream()
-                    .filter(s -> s.getGear_Flt_Grd() != 0).findAny().isPresent())
-                gearFltGrdArray = vcu79CanMessagesSet.parallelStream()
-                        .filter(s -> s.getGear_Flt_Grd() != 0)
-                        .mapToInt(v -> v.getGear_Flt_Grd()).distinct().boxed();
+            if(isGearFltHappened(vcu79CanMessagesSet))
+                gearFltGrdArray = getGearPdlFltGrd(vcu79CanMessagesSet);
             //车速故障等级
-            if(vcu79CanMessagesSet.parallelStream()
-                    .filter(s -> s.getSpd_Flt_Grd() != 0).findAny().isPresent())
-                spdFltGrdArray = vcu79CanMessagesSet.parallelStream()
-                        .filter(s -> s.getSpd_Flt_Grd() != 0)
-                        .mapToInt(v -> v.getSpd_Flt_Grd()).distinct().boxed();
+            if(isSpdFltHappened(vcu79CanMessagesSet))
+                spdFltGrdArray = getSpdFltGrd(vcu79CanMessagesSet);
             //驱动电机故障等级
-            if(vcu79CanMessagesSet.parallelStream()
-                    .filter(s -> s.getDrv_Mtr_Flt_Grd() != 0).findAny().isPresent())
-                drvMtrFltGrdArray = vcu79CanMessagesSet.parallelStream()
-                        .filter(s -> s.getDrv_Mtr_Flt_Grd() != 0)
-                        .mapToInt(v -> v.getDrv_Mtr_Flt_Grd()).distinct().boxed();
+            if(isDrvMtrFltHappened(vcu79CanMessagesSet))
+                drvMtrFltGrdArray = getDrvMtrFltGrd(vcu79CanMessagesSet);
             //DCDC故障等级
-            if(vcu79CanMessagesSet.parallelStream()
-                    .filter(s -> s.getDCDC_Flt_Grd() != 0).findAny().isPresent())
-                dCDCFltGrdArray = vcu79CanMessagesSet.parallelStream()
-                        .filter(s -> s.getDCDC_Flt_Grd() != 0)
-                        .mapToInt(v -> v.getDCDC_Flt_Grd()).distinct().boxed();
+            if(isDCDCFltHappened(vcu79CanMessagesSet))
+                dCDCFltGrdArray = getDCDCFltGrd(vcu79CanMessagesSet);
             //电池故障等级
-            if(vcu79CanMessagesSet.parallelStream()
-                    .filter(s -> s.getBty_Flt_Grd() != 0).findAny().isPresent())
-                btyFltGrdArray = vcu79CanMessagesSet.parallelStream()
-                        .filter(s -> s.getBty_Flt_Grd() != 0)
-                        .mapToInt(v -> v.getBty_Flt_Grd()).distinct().boxed();
+            if(isBtyFltHappened(vcu79CanMessagesSet))
+                btyFltGrdArray = getBtyFltGrd(vcu79CanMessagesSet);
            //汇总故障等级1，2，3
            fltGrdLvl1Num = 0; fltGrdLvl2Num = 0; fltGrdLvl3Num = 0;
             calFltGrdSum(accPdlFltGrdArray);
